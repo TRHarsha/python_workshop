dc28830981c703271c786165656ff1578d963ab5
Harsha T R <63777708+Htr1172@users.noreply.github.com>
2022-11-09 21:52:47 +0530

    Chatboy funny bot
    this is created only for fun.

---
+++ chatboy.py
@@ -0,0 +1,58 @@
+print("Hey! I am Chatboy")
+name=input("What's your name ")
+print("Hey ",name,"\n In what do you need help duh!")
+print("Type 'Purchase' for Purchase related quieries")
+print("Type 'Download' for Download related quieries")
+print("Type 'Update' for Update related quieries")
+print("Type 'Uninstall' for Uninstall related quieries")
+print("Type 'Other' for Other related quieries")
+problem= input("What help you need: ")
+if problem == "Purchase":
+    print("If you are not able to do payment using the link Np! You can pay to my UPI Id: chatboy@idfcbank")
+    print("DON'T FORGET TO SEND THE UTR TO Chatboy@gmail.com from your REGISTERED MAIL ID \n ILLA ANDRE YOUTUBE AL HUDUK GURU YAVANO PIRATED VERSION HAKIRTHANE \n DHUD YAK WASTE MADTHYA NAN UPI KODTHINI KALSBUDU")
+elif problem == "Download":
+    print("Please check your Internet Connection \n Try downloading from the provided link www.chatboy.com/product/directdownload \n Please provide your REGISTERED MAIL ID we will share the required download link")
+    mailid=input("REGISTERED MAIL ID(if solved type NA): ")
+    if mailid == "NA":
+        print("We are happy we could help \n DIALOG HODYAK HELUDRU GURU")
+    else:
+        print("You'll recieve the mail in 5 mins \n SWALPA LATE ADRE BEJAR MADKOBEDA \n ILLA INTERNET ON MADIDYA NODO")
+elif problem == "Update":
+    print("This would not work if you use pirated version \n PURHASE THE ORIGINAL VERSION \n ANTHA HELAK HELIDARE WHATSAPP AL ONDH HI HAKU PIRATED UPDATED VERSION LINK KALSTHINI \n IDN UNINSTALL MAD KALSID LINK INDA DOWNLOAD MADU")
+elif problem == "Uninstall":
+    print("If you dont know this why did u install this bloody fellow, we have the video on our YOUTUBE channel \n PLEASE DO SUBSCRIBE LIKE AND SHARE")
+elif problem == "Other":
+    print("Do send the MAIL at 'chatboy@pestrust.edu.in \n ADRE REPLY MADTHIVI BEJAR MADKOBEDA SWALPA LATE AGUTHE \n NAN KASHTA NU ARTHA MADKO MACHA")
+else:
+    print("NAN HELID ENU NIN TYPE MADIRAD ENU")
+print("Mugitha Problem solve aytha \n Kushi naaa")
+work=input("What are you doing studying or working")
+if work == "working":
+    print("Guru companil ond kelsa kodsu guru. Ond 10 LPA saku")
+    kelsa=input("Kodsthira ilva (yes no)")
+    if kelsa == "yes":
+        print("DEVR GURU NINU")
+    elif kelsa == "no":
+        print("O KELSA IDE ANTHA ATTITUDE AYTH BIDU")
+    else:
+        print("Ning easy agli antha bracket alhakidini \n NODKOND TYPE MADAK BANDILLA ANDRE")
+    madhve=input("Sari Madhve ago idea enadru (yes/no/helalla)")
+    if madhve == "yes":
+        print("Sari Sari Karyad maribeda")
+    elif madhve == "no":
+        print("Yak guru Innu settle agbeka enge")
+    elif madhve == "helalla":
+        print("Nachke nod helake abaabababaa")
+    else:
+        print("Ning easy agli antha bracket alhakidini \n NODKOND TYPE MADAK BANDILLA ANDRE")
+else:
+    print("O NINU NAM THARA NE STRUGLING ")
+    gf=input("HOGLI GF IDARA (yes, no, helalla)")
+    if gf == "yes":
+        print("Insta ID kals guru \n KAL HAKALLA PROMISE")
+    elif gf == "no":
+        print("Namantha olle avrge siggalla \n ARRANGE MARRIAGE GE READY AGU")
+    elif gf == "helalla":
+        print("O Privacy AYTH BID GURU")
+    else:
+        print("Ning easy agli antha bracket alhakidini \n NODKOND TYPE MADAK BANDILLA ANDRE")
