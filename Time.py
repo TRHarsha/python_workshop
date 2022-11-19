d5ffac4591e9f987de72f3b88a4918d2ed2ae46e
Harsha T R <63777708+TRHarsha@users.noreply.github.com>
2022-11-19 14:55:32 +0530

    Stoping Stop watch

--- Time.py
+++ Time.py
@@ -2,18 +2,37 @@ import os
 import time
 second = 0
 minute = 0
-hours = 0
-while (True):
-    print("Simple Stopwatch(in Python) ")
-    print('n')
-    print(hours, minute, second)
-    print('-------------')
-    time.sleep(1)
-    second += 1
-    if (second == 60):
-        second = 0
-        minute += 1
-    if (minute == 60):
-        minute = 0
-        hours += 1
-    os.system('cls')
+hours =0
+stopformat=input("Do you want to stop for second or minute: ")
+if stopformat == "second":
+    insec=int(input("Enter the seconds"))
+    while (second <= insec):
+        print("Simple Stopwatch(in Python) ")
+        print('n')
+        print(hours, minute, second)
+        print('-------------')
+        time.sleep(1)
+        second += 1
+        if (second == 60):
+            second = 0
+            minute += 1
+        if (minute == 60):
+            minute = 0
+            hours += 1
+            os.system('cls')
+elif stopformat == "minute":
+    inmin=int(input("Enter the minutes: "))
+    while (minute <= inmin):
+        print("Simple Stopwatch(in Python) ")
+        print('n')
+        print(hours, minute, second)
+        print('-------------')
+        time.sleep(1)
+        second += 1
+        if (second == 60):
+            second = 0
+            minute += 1
+        if (minute == 60):
+            minute = 0
+            hours += 1
+            os.system('cls')
